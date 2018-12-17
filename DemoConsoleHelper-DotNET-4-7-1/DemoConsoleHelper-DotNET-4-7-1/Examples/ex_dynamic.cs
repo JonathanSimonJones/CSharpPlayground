3031e6f6fda62e921b101ae1dfdfeac220bd4a2b
JJ <jonathansimonjones@gmail.com>
2018-12-17 12:49:27 +0000

    Stable. Working on dynamic type example

---
+++ DemoConsoleHelper-DotNET-4-7-1/DemoConsoleHelper-DotNET-4-7-1/Examples/ex_dynamic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsoleHelper_DotNET_4_7_1.Examples
+{
+    // Reference type
+    // Enables operations to bypass compile-time type checking and instead resolve
+    // type at run time
+    // 
+    class ExDynamic
+    {
+
+    }
+}
