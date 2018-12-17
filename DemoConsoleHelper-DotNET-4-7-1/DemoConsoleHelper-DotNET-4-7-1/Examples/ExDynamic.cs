7564e23412e955b4e8e41fae4fcaf8b80c2fa737
JJ <jonathansimonjones@gmail.com>
2018-12-17 13:43:47 +0000

    Stable. Added base class for examples. Added interfaces for examples. Added demo for dynamic example. Renamed dynamic example to follow coding standard.

--- DemoConsoleHelper-DotNET-4-7-1/DemoConsoleHelper-DotNET-4-7-1/Examples/ExDynamic.cs
+++ DemoConsoleHelper-DotNET-4-7-1/DemoConsoleHelper-DotNET-4-7-1/Examples/ExDynamic.cs
@@ -1,17 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+
+using DemoConsoleHelper_DotNET_4_7_1.Examples.Base;
+using DemoConsoleHelper_DotNET_4_7_1.Examples.Interfaces;
 
 namespace DemoConsoleHelper_DotNET_4_7_1.Examples
 {
     // Reference type
     // Enables operations to bypass compile-time type checking and instead resolve
     // type at run time
-    // 
-    class ExDynamic
+    // Dynamic only exist at compile time, at run time variables of type dynamic 
+    // are compiled into variables of type object
+    class ExDynamic : ExBase
     {
+        private dynamic m_dyn = 3;
+        public dynamic m_obj = 4;
 
+        public ExDynamic()
+        {
+            HelperText = "This is some text.";
+        }
+
+        public override string HelperText
+        {
+            get { return base.m_helperText; }
+            protected set { base.m_helperText = value; }
+        }
+
+
+        public override void Run()
+        {
+            System.Console.WriteLine(this.HelperText);
+            System.Console.WriteLine(m_dyn.GetType());
+            System.Console.WriteLine(m_obj.GetType());
+        }
     }
+
+
 }
