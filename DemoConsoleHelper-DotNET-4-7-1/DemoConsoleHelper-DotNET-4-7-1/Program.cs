831750c606b5a357c725128b5ea1fec6833f5f5a
JJ <jonathansimonjones@gmail.com>
2018-12-17 10:02:35 +0000

    Stable. Initial commit. Added in Hello World program for testing. Added documentation on research done about coding standards and best practices.

---
+++ DemoConsoleHelper-DotNET-4-7-1/DemoConsoleHelper-DotNET-4-7-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsoleHelper_DotNET_4_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Stop immediate termination
+            Console.ReadKey();
+        }
+    }
+}
