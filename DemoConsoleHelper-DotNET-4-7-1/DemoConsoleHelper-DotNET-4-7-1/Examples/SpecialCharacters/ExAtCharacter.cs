858fb0e9d9bbd2b65ae481150352501ebb920d94
JJ <jonathansimonjones@gmail.com>
2018-12-31 12:32:59 +0000

    Stable. Added ExAtCharacter demo. Updated coding standard notes.

---
+++ DemoConsoleHelper-DotNET-4-7-1/DemoConsoleHelper-DotNET-4-7-1/Examples/SpecialCharacters/ExAtCharacter.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoConsoleHelper_DotNET_4_7_1.Examples.Base;
+
+
+namespace DemoConsoleHelper_DotNET_4_7_1.Examples.SpecialCharacters
+{
+    /// <summary>
+    /// Demos usage of the @ chacter
+    /// </summary>
+    public sealed class ExAtCharcter : ExBase
+    {
+        // PUBLIC INTERFACE START
+
+
+        /// <summary>
+        /// Contstructs the example.
+        /// </summary>
+        /// <remarks>
+        /// Where <c cref="HelperText">HelperText</c> is set.
+        /// </remarks>
+        public ExAtCharcter()
+        {
+            string helperText = @"
+@
+special character
+
+Can be used to enable C# keywords to be used as identifiers
+	int @for = 2;
+	Console.Write(@for);
+To indicate a string literal should be intepreted verbatim. 
+To distinguish between attributes in case of a naming conflict.
+
+";
+            HelperText = helperText;
+        }
+
+        /// <summary>
+        /// Returns informative text about the example. 
+        /// </summary>
+        /// <returns>
+        /// A string with helpful text about the example in question. 
+        /// </returns>
+        public override string HelperText
+        {
+            get => m_helperText;
+            protected set => m_helperText = value;
+        }
+
+        /// <summary>
+        /// Returns text to be displayed by a console. 
+        /// </summary>
+        /// <remarks>
+        /// Outputs contents of <c cref="ConsoleTextBuilder">ConsoleTextBuilder</c>
+        /// </remarks>
+        /// <returns>
+        /// The text that the console should display
+        /// </returns>
+        public override string ConsoleText
+        {
+            get => ConsoleTextBuilder.ToString();
+        }
+
+        // PUBLIC INTERFACE END
+
+
+
+
+
+        // PROTECTED INTERFACE START
+
+        /// <summary>
+        /// Workhorse of the class.
+        /// </summary>
+        /// <remarks>
+        /// No need to call. Is run from base class constructor. 
+        /// </remarks>
+        protected override void Run()
+        {
+            int @int = 0;
+
+            ConsoleTextBuilder.AppendLine($"The value of the variable idenified as int is: " +
+                                          $"{@int}");
+
+            string verbatimStringExample = @"some 
+            verbatim
+        string
+";
+            ConsoleTextBuilder.AppendLine(verbatimStringExample);
+
+
+        }
+
+        /// <summary>
+        /// Gives access to the object used to constuct data to pass to the console. 
+        /// </summary>
+        /// <remarks>
+        /// Uses a StringBuilder object.
+        /// </remarks>
+        protected override StringBuilder ConsoleTextBuilder
+        {
+            get => m_consoleTextBuilder; 
+        }
+
+        // PROTECTED INTERFACE END
+
+
+
+        // PRIVATE INTERFACE START
+
+
+        // PRIVATE INTERFACE END
+
+
+
+
+
+        // MEMBER VARIABLES START
+
+        private string m_helperText;
+        private StringBuilder m_consoleTextBuilder = new StringBuilder();
+
+        // MEMBER VARIABLES END
+
+
+    }
+}
